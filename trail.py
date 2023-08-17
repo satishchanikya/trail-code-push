300af43048c57767bfe60402df0c08aef0f12988
satishchanikya <mallachanikya22@gmail.com>
2023-08-17 23:00:59 +0530

    trail code

---
+++ trail.py
@@ -0,0 +1,9 @@
+print("hello world....!")
+# This code is normal python code. 
+# Just trailing the the code push into the git.
+# creaate repo
+# all the process in the terminal 
+# open the terminal and set your local project as a current working directory.--> pwd
+# 
+    
+    
