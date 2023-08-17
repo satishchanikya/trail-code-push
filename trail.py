659fd09f3bd60b5f6ae295e93653ec64716e0862
satishchanikya <mallachanikya22@gmail.com>
2023-08-17 23:16:53 +0530

    first commit

--- trail.py
+++ trail.py
@@ -4,6 +4,11 @@ print("hello world....!")
 # creaate repo
 # all the process in the terminal 
 # open the terminal and set your local project as a current working directory.--> pwd
-# 
+# type 'ls' and after your directory is present here.
+# git init --> initilized the repo with the .git folder
+# git add . 
+# git status --> to see the comitted file in green color
+# git commit -m 'first trail code'
+
     
     
